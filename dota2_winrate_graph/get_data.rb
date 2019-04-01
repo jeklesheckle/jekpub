ea17fd8989a16f5f8ba04564b6d5b8ac47ee1ab5
Laptop win10 <jackpielstick@gmail.com>
2019-04-01 18:15:27 -0400

    more progress less understanding

--- dota2_winrate_graph/get_data.rb
+++ dota2_winrate_graph/get_data.rb
@@ -1,10 +1,11 @@
-# This program will fetch the data from Steam's servers using their WebApi
-# via Net::HTTP
+# uses Steam's WebAPI to fetch data about the 500 most recent all pick matches
+# and finds the winrate of each side (dire/radiant)
 
 =begin
 Links:
 =======
 
+WebApi page of TF2 wiki: https://wiki.teamfortress.com/wiki/WebAPI
 Steam WebAPI guide: https://steamwebapi.azurewebsites.net/
 HTTParty doc: https://github.com/jnunemaker/httparty/tree/master/docs
 =end
@@ -15,8 +16,8 @@ Developer notes:
 
 API key: 37FB080E73DCCC7BA31BC11B52C5C453
 
-Current status: just figured out how to do requests in browser. need to do more
-testing in code
+Current status: gonna need to use GetMatchDetails instead to get who won.
+unsure of how to parse the results (I believe they are strings)
 =end
 
 require 'httparty'
@@ -25,6 +26,7 @@ require 'httparty'
 # https://{base_url}/{interface}/{method}/{version}?{parameters}
 # parameters are delimited with '&'
 response = HTTParty.get(  # needed 570 in place of <ID> since dota's ID is 570"
-  'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=37FB080E73DCCC7BA31BC11B52C5C453&game_mode=16'
+  'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=37FB080E73DCCC7BA31BC11B52C5C453&game_mode=1'
 )
-puts response
+
+puts response.body
