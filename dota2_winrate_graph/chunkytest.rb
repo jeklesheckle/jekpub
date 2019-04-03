026857d6510fc88c34e67cb2b4b9c006f7515325
jeklesheckle <jackpielstick@gmail.com>
2019-04-02 23:58:30 -0400

    tested chunkytest, added instructions

--- dota2_winrate_graph/chunkytest.rb
+++ dota2_winrate_graph/chunkytest.rb
@@ -1,25 +1,62 @@
 # just a little ruby file to test out coloring in chunkypng
 
+=begin
+Current TODO:
+* add the rest of the characters
+* scale them up by a factor of 4
+* add the functions for the xaxis
+* add the function for the graph bars 
+* transfer everything over to get_data
+=end
+
 require 'chunky_png'
+
+# starting at the top left of an image, true if 
+# a pixel is filled in, false otherwise 
+all_characters = {
+"big_r" => [true, true, true, false,
+			true, false, false, true,
+			true, false, false, true,
+			true, true, true, false,
+			true, false, false, true,
+			true, false, false, true]
+}
 
 # graphs the x-axis of the bar graph 
 def graph_x_axis()
 
 end
 
-# graph a character given the character, a png 
+# graph a character given the character, a png
 # and a top-left location for the character
 # each character is 4 pixels long and 6 tall
 # If input is invalid, will return a message
 # stating and will not alter the png
 # returns true if the char was graphed, false
 # otherwise.
-def png_char(png_object, character, start_x, start_y)
+# default color is pure black.
+def add_char_to_png(png_obj, characters, character, start_x, start_y, color = nil)
 	#check to see if character is one I've precoded
+	
+	pixel_map = characters[character]
+	if pixel_map == nil then return false end
 	
 	#check to see if you'll go out of bounds
 	
 	
+	#actually edit the png 
+	if color == nil then color = :black end
+	
+	# iterate through the size of a char (4 x 6), filling in
+	# the pixels for the char with color 
+	(0...4).each do |row|
+		(0...6).each do |col|
+			px_map_i = col * 4 + row
+			if pixel_map[px_map_i] then 
+				png_obj[start_x + row, start_y + col] = color
+			end
+		end
+	end
 end
 
 width = 250
@@ -30,14 +67,7 @@ height = 550
 
 png = ChunkyPNG::Image.new(width, height, 0x333333ff)
 
-border_thickness = 5
-
-(0...width).each do |x_coord|
-	(0...border_thickness).each do |y_coord|
-		png[x_coord, y_coord] = :black
-	end
-end
-
+add_char_to_png(png, all_characters, "big_r", 50, 50)
 
 png.save('chunkytest.png', :interlace => true)
 
