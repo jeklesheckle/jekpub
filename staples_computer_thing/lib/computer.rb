e6736b46fc17861d5237ac116bd5cd71bf0d8e0b
Jack Pielstick <jackpielstick@gmail.com>
2019-06-25 15:50:29 -0400

    move serialization to marshal
    using marshal for serialization now, much better in every way.
    
    also created directory test for file test.rb which creates a sample computer
    and tests the methods on it.
    
    currently there is a bug with the way i am storing the object, not sure
    which file it's in but it will be an easy fix (name is the processor hash
    for some reason)

--- staples_computer_thing/lib/computer.rb
+++ staples_computer_thing/lib/computer.rb
@@ -4,11 +4,12 @@
 # H E L P E R  F U N C T I O N S
 ######################################################################
 # takes a string and returns true/false based on what the string spells
+
 def to_bool(string)
   capstring = string.to_s.upcase
   if capstring == "TRUE" then
     return true
-  else if capstring == "FALSE" then
+  elsif capstring == "FALSE" then
     return false
   else
     raise "Argument error: to_bool expected true or false but was given something else"
@@ -20,103 +21,48 @@ end
 # A C T U A L  C O M P U T E R  S T U F F
 ######################################################################
 # takes a computer and stores it into the database as a line of characters
-def store_computer(computer)
+def store_computer(computer_to_store)
+  # check to make sure file exists
+  if File.exists?("computers.cstore") then
+    # open file
+    computers_file_r = File.open("computers.cstore")
 
+    # read it into an array of Computers (restore it using Marshal)
+    computers_arr = Marshal.restore(computers_file_r)
+    computers_file_r.close
+
+    # find if the computer already exists by parsing that var (remove it if so)
+    computers_arr.delete_if {|curr_computer| curr_computer.name == computer_to_store.name}
+
+    # store the computer in the array (computer locations are arbitrary)
+    computers_arr << computer_to_store
+
+    # serialize and save the file
+    computers_file_w = File.open("computers.cstore", "w")
+    Marshal.dump(computers_arr, computers_file_w)
+  else
+    computers_file_w = File.open("computers.cstore", "w")
+    Marshal.dump([computer_to_store], computers_file_w)
+  end
+
+  computers_file_w.close
 end
+
 
 # returns the computer object named in the args from file storage
 def load_computer(name)
-  File.open("computers.cstor", "r") do |file|
-    #match the name then match and get the params
-    while(line = file.gets) do
-      # captures the stored objects
-      line.match(/#{name}|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|/) do |entry|
-        # parsing the stored object strings for individual fields
-        # fields are delimited by % symbols
-        # 0: os, 1: processor, 2: storage, 3: memory, 4: screen, 5: battery,
-        # 6: keyboard, 7: display_ports, 8: other_ports, 9: upgrade_specs,
-        # 10: peripherals
-        caps = entry.captures
-
-        details_hash = Hash.new
-        details_hash["os"] = caps[0]
-
-        # may want a check to see if string is valid
-        # will need to confirm that nils are generated rather than ""
-        processor_fields = caps[0].split("%")
-        processor_hash = Hash.new
-        processor_hash["name"] = processor_fields[0].to_s
-        processor_hash["base_clock"] = processor_fields[1].to_f
-        processor_hash["boost_clock"] = processor_fields[2].to_f
-        processor_hash["cores"] = processor_fields[3].to_i
-        processor_hash["threads"] = processor_fields[4].to_i
-        processor_hash["hyperthreading?"] = to_bool(processor_fields[5])
-        processor_hash["graphics"] = processor_fields[6].to_s
-        processor_hash["TDP"] = processor_fields[7].to_i
-        processor_hash["lithography"] = processor_fields[8].to_i
-        details_hash["processor"] = processor_hash
-
-        storage_fields = caps[1].split("%")
-        storage_hash = Hash.new
-        storage_hash["size"] = storage_fields[0].to_i
-        storage_hash["type"] = storage_fields[1].to_s
-        storage_hash["rpm"] = storage_fields[2].to_i
-        details_hash["storage"] = storage_hash
-
-        memory_fields = caps[2].split("%")
-        memory_hash = Hash.new
-        memory_hash["size"] = memory_fields[0].to_i
-        memory_hash["speed"] = memory_fields[1].to_i
-        memory_hash["type"] = memory_fields[2].to_i
-        memory_hash["slots_used"] = memory_fields[3].to_i
-        memory_hash["slots_available"] = memory_fields[4].to_i
-        details_hash["memory"] = memory_hash
-
-        screen_fields = caps[3].split("%")
-        screen_hash = Hash.new
-        screen_hash["size"] = screen_fields[0].to_f
-        screen_hash["resolution"] = screen_fields[1].split("$")
-        screen_hash["touch?"] = to_bool(screen_fields[2])
-        details_hash["screen"] = screen_hash
-
-        battery_fields = caps[4].split("%")
-        battery_hash = Hash.new
-        battery_hash["life_estimate"] = battery_fields[0].to_i
-        battery_hash["mAH"] = battery_fields[1].to_i
-        details_hash["battery"] = battery_hash
-
-        keyboard_fields = caps[5].split("%")
-        keyboard_hash = Hash.new
-        keyboard_hash["backlit?"] = to_bool(keyboard_fields[0])
-        keyboard_hash["numpad?"] = to_bool(keyboard_fields[1])
-        details_hash["keyboard"] = keyboard_hash
-
-        details_hash["display_ports"] = caps[6].split("$") # arr elements del. by $
-
-        details_hash["other_ports"] = caps[7].split("$")
-
-        upgrade_traits = caps[8].split("%")
-        upgrade_hash = Hash.new
-        upgrade_hash["max_ram"] = upgrade_traits[0].to_i
-        upgrade_hash["hard_drive_easy?"] = to_bool(upgrade_traits[1])
-        upgrade_hash["processor_soldered?"] = to_bool(upgrade_traits[2])
-        details_hash["upgrade"] = upgrade_hash
-
-        details_hash["peripherals"] = caps[9].split("$")
-
-        return create_computer(details_hash)
-      end
-    end
-    return nil
-  end
+  return Marshal.restore(File.open("computers.cstore", "r+"))
 end
+
+
+# prints the computer's contents to console
 
 # creates a new empty computer
 # takes a hash of computer traits that contains a mapping from strings
 # describing the detail to the value of that detail.
 def create_computer(details_hash)
-  return Computer.new(details_hash["processor"], details_hash["storage"],
-    details_hash["memory"], details_hash["screen"], details_hash["battery"]),
+  return Computer.new(details_hash["os"], details_hash["processor"], details_hash["storage"],
+    details_hash["memory"], details_hash["screen"], details_hash["battery"],
     details_hash["keyboard"], details_hash["display_ports"], details_hash["other_ports"],
     details_hash["upgrade_specs"], details_hash["peripherals"])
 end
@@ -128,14 +74,14 @@ Traits:
 os: [String] The operating system the device comes with
 processor: [Hash] Processor traits:
       "name" -> [String] The model of processor
-      "base_clock" -> [Float] The base clock speed
-      "boost_clock" -> [Float] The boost clock speed
+      "base_clock" -> [Float] The base clock speed in gigahertz
+      "boost_clock" -> [Float] The boost clock speed in gigahertz
       "cores" -> [Int] Number of cores
       "threads" -> [Int] Number of threads
       "hyperthreading?" -> [Bool] Whether an Intel processor has hyperthreading
       "graphics" -> [String] Onboard graphics
       "TDP" -> [Int] Power draw in Watts
-      "lithography" -> [String] Lithography (die size in nanometers)
+      "lithography" -> [int] Lithography (die size in nanometers)
 storage: [Hash] Storage traits:
       "size" -> [Int] Size in gigabytes
       "type" -> [String] Type of storage (HDD3.5, SSD2.5, eMMC, etc.)
@@ -171,8 +117,8 @@ peripherals: [Array] Strings describing peripherals included in the box
 class Computer
   # each of these member vars can be an object/struct with several members
   # e.g. processor has base_clock, boost_clock, cores, etc.
-  attr_accessor :os, :processor, :storage, :memory, :screen, :battery, :keyboard
-    , :display_ports, :other_ports, :upgrade_specs, :peripherals
+  (attr_accessor :os, :processor, :storage, :memory, :screen, :battery, :keyboard,
+  :display_ports, :other_ports, :upgrade_specs, :peripherals)
 
   attr_reader :name
 
---
+++ staples_computer_thing/test/test.rb
@@ -0,0 +1,80 @@
+# this file is for live testing, may have other files for unit testing later
+
+require '../lib/computer.rb'
+
+
+## this does not use the object creation methodology
+# that i want to follow in the future, i want to create
+# objects in the command line and eventually using the scraper
+# in synchrony with a gui
+##############################################################
+#####         S A M P L E  C O M P U T E R          ##########
+##############################################################
+details_hash = Hash.new
+details_hash["os"] =
+
+# may want a check to see if string is valid
+# will need to confirm that nils are generated rather than ""
+processor_hash = Hash.new
+processor_hash["name"] = "jektel i69"
+processor_hash["base_clock"] = 5.0
+processor_hash["boost_clock"] = 6.9
+processor_hash["cores"] = 4
+processor_hash["threads"] = 8
+processor_hash["hyperthreading?"] = true
+processor_hash["graphics"] = "jektel hd420"
+processor_hash["TDP"] = 2
+processor_hash["lithography"] = 6
+details_hash["processor"] = processor_hash
+
+storage_hash = Hash.new
+storage_hash["size"] = 9
+storage_hash["type"] = "NVMe"
+storage_hash["rpm"] = 0
+details_hash["storage"] = storage_hash
+
+memory_hash = Hash.new
+memory_hash["size"] = 16
+memory_hash["speed"] = 2400
+memory_hash["type"] = "DDR4"
+memory_hash["slots_used"] = 2
+memory_hash["slots_available"] = 4
+details_hash["memory"] = memory_hash
+
+screen_hash = Hash.new
+screen_hash["size"] = 15.6
+screen_hash["resolution"] = [1080, 720]
+screen_hash["touch?"] = false
+details_hash["screen"] = screen_hash
+
+battery_hash = Hash.new
+battery_hash["life_estimate"] = 10
+battery_hash["mAH"] = 10000
+details_hash["battery"] = battery_hash
+
+keyboard_hash = Hash.new
+keyboard_hash["backlit?"] = true
+keyboard_hash["numpad?"] = true
+details_hash["keyboard"] = keyboard_hash
+
+details_hash["display_ports"] = ["HDMI", "DVI"]
+
+details_hash["other_ports"] = ["USB-C", "USB3.1", "SD"]
+
+upgrade_hash = Hash.new
+upgrade_hash["max_ram"] = 64
+upgrade_hash["hard_drive_easy?"] = true
+upgrade_hash["processor_soldered?"] = false
+details_hash["upgrade"] = upgrade_hash
+
+details_hash["peripherals"] = []
+
+sample_computer = create_computer(details_hash)
+
+########################### ACTUALLY DOING THINGS WITH SAMPLE
+store_computer(sample_computer)
+comp_obj = load_computer(sample_computer)
+
+puts comp_obj.inspect
+
+#### current bug is just that store / the hash isn't set up right, ezpc fix
