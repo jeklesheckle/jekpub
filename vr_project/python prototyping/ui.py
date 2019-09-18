2bc5f4fee462a27cd8dd2f4f48f724e8e52ef8e3
Jek Desktop Win10 <jackpielstick@gmail.com>
2019-09-18 09:15:44 -0400

    follow tkinter tutorial

---
+++ vr_project/python prototyping/ui.py
@@ -0,0 +1,16 @@
+# this is the first file for my VR life app. idk what ill call it yet
+
+# spose I should write the main menu first
+
+from tkinter import *
+
+window = Tk()
+
+label = Label(window, text="Hello, world!", font=("Comic Sans MS", 69))
+#label.grid(column=0, row=0) # numbers dont seem to matter here yet
+
+window.title("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+label.pack()
+
+window.mainloop()
